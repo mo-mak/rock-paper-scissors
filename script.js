9f01a3800c2152a3d5c4adde42f3ef91c30f4ac5
momak <moh.makarehchi@gmail.com>
2023-03-09 20:53:22 -0500

    Add singleGame function
    - This function will play a single game using a playerSelection argument.
    - It will call the getComputerChoice function and store the value in a
    variable named computerChoice (the same name as the return value of
    the getComputerChoice function).
    - It will then compare the playerSelection with computerChoice in a
    tedious tree of if and else if statements and print the appropriate
    result.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoise() {
+function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     let computerChoice = '';
     
@@ -12,4 +12,42 @@ function getComputerChoise() {
     
     return computerChoice;
 }
+
+function singleGame(playerSelection) {
+    
+    playerSelection = playerSelection.slice().toLowerCase(); //make string lower case
+    let computerChoice = getComputerChoice() 
+    computerChoice = computerChoice.slice().toLowerCase(); //make string lower case
+    
+    //rock vs paper
+    if (playerSelection == 'rock' && computerChoice == 'paper') { 
+        console.log('You lose! Computer selected paper. Paper beats Rock...');
+    
+    //rock vs scissors
+    } else if (playerSelection == 'rock' && computerChoice == 'scissors') { 
+        console.log('You win! Computer selected scissors. Rock beats Scissors!');
+    
+    //paper vs rock
+    } else if (playerSelection == 'paper' && computerChoice == 'rock') {
+        console.log('You win! Computer selected rock. Paper beats Rock!');
+    
+    //paper vs scissors
+    } else if (playerSelection == 'paper' && computerChoice == 'scissors') {
+        console.log('You lose! Computer selected scissors. Scissors beats Paper!');
+    
+    //scissors vs rock
+    } else if (playerSelection == 'scissors' && computerChoice == 'rock') {
+        console.log('You lose! Computer selected rock. Rock beats Scissors!');
+    
+    //scissors vs paper
+    } else if (playerSelection == 'scissors' && computerChoice == 'paper') {
+        console.log('You win! Computer selected paper. Scissors beats Paper!');
+    
+    //draw
+    } else if (playerSelection == computerChoice) {
+        console.log("'Oy! It's a draw!'");
+    }
+}
+
+console.log(singleGame('paper'))
 
